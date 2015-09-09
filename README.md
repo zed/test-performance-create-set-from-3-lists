fa660c4a76b698dd248851d153020e4fa2d64f6c
zed <isidore.john.r@gmail.com>
2015-09-09 19:41:34 +0300

    add benchmark code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# test-performance-create-set-from-3-lists
-Measure time performance of creating a set from 3 lists.
+# [Why is creating a set from a concatenated list faster than using `.update`?](http://stackoverflow.com/q/32483539/4279)
+
+[![build status on travis-ci.org][2]][1]
+
+[1]: https://travis-ci.org/zed/test-performance-create-set-from-3-lists
+[2]: https://api.travis-ci.org/zed/test-performance-create-set-from-3-lists.svg?branch=master
+
+To measure time performance of creating a set from 3 lists, run `tox`.
---
+++ __main__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Measure time performance of creating a set from 3 lists."""
+from __future__ import print_function
+import os
+import random
+import sys
+import subprocess
+
+seed = random.randrange(1, 4294967296)
+env = dict(os.environ, PYTHONHASHSEED=str(seed))
+print("PYTHONHASHSEED=%d" % seed)
+for code in [
+        'set(A+B+C)',
+        'set(A).update(B, C)',
+        'set(itertools.chain(A,B,C))']:
+    print(code, end=': ')
+    sys.stdout.flush()
+    subprocess.check_call([sys.executable, '-m', 'timeit', '-s',"""
+import itertools
+import random
+random.seed(%d)
+n = 1000000
+A = [random.randrange(1<<30) for _ in range(n)]
+B = [random.randrange(1<<30) for _ in range(n)]
+C = [random.randrange(1<<30) for _ in range(n)]""" % seed, code], env=env)
